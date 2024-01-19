fff47b8f51743f4020ed0b9647a02a563153e2b7
yang jaehyun <jaehyun.yang@bespinglobal.com>
2024-01-19 09:39:12 +0900

    cleansing

--- 10-terraform/vpc-eks-apply.sh
+++ 10-terraform/vpc-eks-apply.sh
@@ -1,45 +1,45 @@
 !/bin/bash
-# echo "********************** Make AWS credentials Setting ***********************"
-# aws configure
+echo "********************** Make AWS credentials Setting ***********************"
+aws configure
 
-# echo "********************** Show AWS credentials  ***********************"
-# echo ~/.aws/credentials
-# echo "********************************************************************"
-
-
-# echo "********************** Terraform install ***********************"
-# echo "테라폼 cli 설치가 필요하시면 y를 입력해주세요"
-# read terra_y_value
-# if [ "$terra_y_value" == "y" ]; then
-#     echo "terraform cli 설치를 시작합니다."
-#     wget https://releases.hashicorp.com/terraform/0.14.7/terraform_0.14.7_linux_amd64.zip
-#     unzip terraform_0.14.7_linux_amd64.zip
-#     sudo mv terraform /usr/local/bin/
-#     terraform --version
-# else
-#     echo "설치는 pass"
-# fi
-
-# echo "********************** Terraform install finish ***********************"
+echo "********************** Show AWS credentials  ***********************"
+echo ~/.aws/credentials
+echo "********************************************************************"
 
 
-# echo "********************** keypair Make Start ***********************"
-# echo "ssh-keygen key를 생성합니다. key file 이름을 입력해주세요. EX) jaehyun.ynag "
-# read kegen_y_value
-# ssh-keygen -t rsa -b 2048 -m pem -f ${kegen_y_value}-keypair
+echo "********************** Terraform install ***********************"
+echo "테라폼 cli 설치가 필요하시면 y를 입력해주세요"
+read terra_y_value
+if [ "$terra_y_value" == "y" ]; then
+    echo "terraform cli 설치를 시작합니다."
+    wget https://releases.hashicorp.com/terraform/0.14.7/terraform_0.14.7_linux_amd64.zip
+    unzip terraform_0.14.7_linux_amd64.zip
+    sudo mv terraform /usr/local/bin/
+    terraform --version
+else
+    echo "설치는 pass"
+fi
 
-# echo  " 만들어진 공개키는 아래와 같습니다 "
-# cat ${kegen_y_value}-keypair.pub
-# echo  " 만들어진 개인키는 아래와 같습니다 "
-# cat ${kegen_y_value}-keypair
+echo "********************** Terraform install finish ***********************"
 
-# echo " 생성된 개인키와 공개키는 테라폼 구성을 위해 ./arch/terraform-middle/dev/10-pre-requisite/template/ 경로로 복사합니다"
-# cp ${kegen_y_value}-keypair ./arch/terraform-middle/dev/10-pre-requisite/template/
-# cp ${kegen_y_value}-keypair.pub ./arch/terraform-middle/dev/10-pre-requisite/template/
 
-# rm ${kegen_y_value}-keypair
-# rm ${kegen_y_value}-keypair.pub
-# echo "********************** keypair Make Finish ***********************"
+echo "********************** keypair Make Start ***********************"
+echo "ssh-keygen key를 생성합니다. key file 이름을 입력해주세요. EX) jaehyun.ynag "
+read kegen_y_value
+ssh-keygen -t rsa -b 2048 -m pem -f ${kegen_y_value}-keypair
+
+echo  " 만들어진 공개키는 아래와 같습니다 "
+cat ${kegen_y_value}-keypair.pub
+echo  " 만들어진 개인키는 아래와 같습니다 "
+cat ${kegen_y_value}-keypair
+
+echo " 생성된 개인키와 공개키는 테라폼 구성을 위해 ./arch/terraform-middle/dev/10-pre-requisite/template/ 경로로 복사합니다"
+cp ${kegen_y_value}-keypair ./arch/terraform-middle/dev/10-pre-requisite/template/
+cp ${kegen_y_value}-keypair.pub ./arch/terraform-middle/dev/10-pre-requisite/template/
+
+rm ${kegen_y_value}-keypair
+rm ${kegen_y_value}-keypair.pub
+echo "********************** keypair Make Finish ***********************"
 
 
 echo "********************** terraform tfvars owner 변수를 변경합니다 ****************************"
