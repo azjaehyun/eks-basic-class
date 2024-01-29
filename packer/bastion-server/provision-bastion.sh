c8bc788fe64c3db562a86abd66f1884382b26613
yang jaehyun <jaehyun.yang@bespinglobal.com>
2024-01-29 16:04:09 +0900

    env copy

--- packer/bastion-server/provision-bastion.sh
+++ packer/bastion-server/provision-bastion.sh
@@ -64,6 +64,10 @@ aws-iam-authenticator version
 K9S_VERSION=v0.26.7
 curl -sL https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_Linux_x86_64.tar.gz | sudo tar xfz - -C /usr/local/bin   
 
+## install terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
 
 ## env setting & git clone 
 mkdir ~/.kube
