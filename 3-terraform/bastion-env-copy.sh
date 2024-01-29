6d1ba6ada96beda46d5091faf6fb6aa8619d2449
yang jaehyun <jaehyun.yang@bespinglobal.com>
2024-01-29 09:41:30 +0900

    clenasing

--- 3-terraform/bastion-env-copy.sh
+++ 3-terraform/bastion-env-copy.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+echo "********************** bastion에 env 파일을 복사합니다. ***********************"
+echo "********************** 복사대상은 ssh private key , terraform tfstate file 을 복사합니다. ***********************"
 
+read bastion_public_ip
 
+echo "********************** bastion에 ssh private key  파일을 복사합니다. ***********************"
+scp -i yangjaehyun-keypair yangjaehyun-keypair  ubuntu@${bastion_public_ip}:~/.ssh
+echo "********************** bastion에 10-pre-requisite terraform.tfstate 파일을 복사합니다. ***********************"
+scp -i yangjaehyun-keypair ./arch/terraform-middle/dev/10-pre-requisite/terraform.tfstate ubuntu@${bastion_public_ip}:~/.ssh
+echo "********************** bastion에 20-vpc-subnet-eks-bastion terraform.tfstate 파일을 복사합니다. ***********************"
+scp -i yangjaehyun-keypair ./arch/terraform-middle/dev/20-vpc-subnet-eks-bastion/terraform.tfstate ubuntu@${bastion_public_ip}:~/.ssh
+echo "********************** bastion에 40-eks-getting-started terraform.tfstate  파일을 복사합니다. ***********************"
+scp -i yangjaehyun-keypair ./arch/terraform-middle/dev/40-eks-getting-started/terraform.tfstate ubuntu@${bastion_public_ip}:~/.ssh
 
-scp -i yangjaehyun-keypair yangjaehyun-keypair  ubuntu@35.92.203.68:~/.ssh
- ./arch/terraform-middle/dev/10-pre-requisite/
