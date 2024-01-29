7771ad3d633751154cc728f5e9c02b3382da40b7
yang jaehyun <jaehyun.yang@bespinglobal.com>
2024-01-29 09:46:12 +0900

    clenasing

--- 3-terraform/bastion-env-copy.sh
+++ 3-terraform/bastion-env-copy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 echo "********************** bastion에 env 파일을 복사합니다. ***********************"
 echo "********************** 복사대상은 ssh private key , terraform tfstate file 을 복사합니다. ***********************"
+echo "********************** 복사를 위해 ssh private key file 이름을 입력해주세요. ***********************"
+read ssh_private_key
 echo "********************** 복사를 위해 bastion public ip를 입력해주세요. ***********************"
 read bastion_public_ip
 
 echo "********************** bastion에 ssh private key  파일을 복사합니다. ***********************"
-scp -i yangjaehyun-keypair yangjaehyun-keypair  ubuntu@${bastion_public_ip}:~/.ssh
+scp -i ${ssh_private_key} ${ssh_private_key}  ubuntu@${bastion_public_ip}:~/.ssh
 echo "********************** bastion에 10-pre-requisite terraform.tfstate 파일을 복사합니다. ***********************"
-scp -i yangjaehyun-keypair ./arch/terraform-middle/dev/10-pre-requisite/terraform.tfstate ubuntu@${bastion_public_ip}:~/.ssh
+scp -i ${ssh_private_key} ./arch/terraform-middle/dev/10-pre-requisite/terraform.tfstate ubuntu@${bastion_public_ip}:~/eks-basic-class/3-terraform/arch/terraform-middle/dev/10-pre-requisite/terraform.tfstate
 echo "********************** bastion에 20-vpc-subnet-eks-bastion terraform.tfstate 파일을 복사합니다. ***********************"
-scp -i yangjaehyun-keypair ./arch/terraform-middle/dev/20-vpc-subnet-eks-bastion/terraform.tfstate ubuntu@${bastion_public_ip}:~/.ssh
+scp -i ${ssh_private_key} ./arch/terraform-middle/dev/20-vpc-subnet-eks-bastion/terraform.tfstate ubuntu@${bastion_public_ip}:~/eks-basic-class/3-terraform/arch/terraform-middle/dev/20-vpc-subnet-eks-bastion/terraform.tfstate
 echo "********************** bastion에 40-eks-getting-started terraform.tfstate  파일을 복사합니다. ***********************"
-scp -i yangjaehyun-keypair ./arch/terraform-middle/dev/40-eks-getting-started/terraform.tfstate ubuntu@${bastion_public_ip}:~/.ssh
+scp -i ${ssh_private_key} ./arch/terraform-middle/dev/40-eks-getting-started/terraform.tfstate ubuntu@${bastion_public_ip}:~/eks-basic-class/3-terraform/arch/terraform-middle/dev/40-eks-getting-started/terraform.tfstate
 
